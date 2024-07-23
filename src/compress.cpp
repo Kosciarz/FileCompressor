b5e8903f79b356e25d697286139a1a5dbefc6d2f
Kosciarz <bartoszbachul1@gmail.com>
2024-07-23 08:19:44 +0200

    ostatnie poprawki. zmiany w sposobie kompresji

--- src/compress.cpp
+++ src/compress.cpp
@@ -13,12 +13,26 @@ static std::fstream openFileToWrite();
 static std::string loadDataToCompress(std::fstream file);
 static std::vector<int> loadCodesToDecompress(std::fstream file);
 static void compressFile(const std::string &data);
-static void decompressFile(const std::vector<int> &codes);
+static void decompressFile(std::vector<int> codes);
 
 void startProgram(int argc, char *argv[])
 {
-    std::string operation = static_cast<std::string>(argv[1]);
-    fileName = static_cast<std::string>(argv[2]);
+    std::string operation;
+    if (argc == 1)
+    {
+        std::cerr << "Nie podano pliku" << std::endl;
+        exit(1);
+    }
+    else if (argc == 2)
+    {
+        operation = "-c";
+        fileName = static_cast<std::string>(argv[1]);
+    }
+    else
+    {
+        operation = static_cast<std::string>(argv[1]);
+        fileName = static_cast<std::string>(argv[2]);
+    }
     std::fstream file = openFileToRead();
 
     if (operation == "-c")
@@ -38,12 +52,10 @@ static void compressFile(const std::string &data)
     //  I jeszcze obsługę że podaje nazwę pliku jako
     //  argument przy kompilacji i wybieram czy chcę
     //  skompresować czy zdekompresować.
-
     static int dictSize = 256;
     std::map<std::string, int> dict;
     for (int i = 0; i < dictSize; i++)
         dict[{static_cast<char>(i)}] = i;
-
     std::vector<int> codes;
     std::string sequnce = "", charsToAdd;
     char character;
@@ -62,41 +74,38 @@ static void compressFile(const std::string &data)
     }
     if (!sequnce.empty())
         codes.push_back(dict[sequnce]);
-
     std::fstream file = openFileToWrite();
     for (int const &code : codes)
         file << code << std::endl;
     file.close();
 }
 
-static void decompressFile(const std::vector<int> &codes)
+static void decompressFile(std::vector<int> codes)
 {
+    std::fstream file = openFileToWrite();
     static int dictSize = 256;
     std::map<int, std::string> dict;
     for (int i = 0; i < dictSize; i++)
         dict[i] = {static_cast<char>(i)};
-
-    std::string decompressedData;
-    std::string pes;
-
+    std::string entry;
+    int lastCode = codes[0];
+    codes.erase(codes.begin());
+    file << dict[lastCode];
     for (const int &code : codes)
     {
-        if (dict.contains(code))
+        entry = dict[lastCode];
+        if(dict.contains(code))
         {
-            decompressedData += dict[code];
-            pes += dict[code][0];
-            dict[dictSize++] = pes;
+            dict.insert({dictSize++, entry + dict[code][0]});
+            file << dict[code];
         }
-        else
+        else 
         {
-            std::string v = pes + dict[code][0];
-            dict[dictSize++] = v;
-            decompressedData += v;
+            dict.insert({dictSize++, entry + entry[0]});
+            file << entry + entry[0];
         }
+        lastCode = code;
     }
-    
-    std::fstream file = openFileToWrite();
-    file << decompressedData;
     file.close();
 }
 
@@ -104,11 +113,8 @@ static std::fstream openFileToRead()
 {
     std::fstream file;
     file.open(fileName, std::fstream::in);
-
     if (!file.is_open())
-    {
         std::cerr << "Error: could not open the file!" << std::endl;
-    }
     return file;
 }
 
@@ -116,11 +122,8 @@ static std::fstream openFileToWrite()
 {
     std::fstream file;
     file.open(fileName, std::fstream::out | std::fstream::trunc);
-
     if (!file.is_open())
-    {
         std::cerr << "Error: could not open the file!" << std::endl;
-    }
     return file;
 }
 
@@ -128,7 +131,7 @@ static std::string loadDataToCompress(std::fstream file)
 {
     std::string data, fileLine;
     while (std::getline(file, fileLine))
-        data += fileLine;
+        data += fileLine + "\n";
     return data;
 }
 
